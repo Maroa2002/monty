4ff5c464e7ee6777725a71f169db6144cefa15ee
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-09-21 12:17:51 +0300

    monty 7

--- operations.c
+++ operations.c
@@ -58,14 +58,14 @@ void monty_div(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		printf("L%u: can't div, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 		free_mem(*stack);
 		exit(EXIT_FAILURE);
 	}
 
 	if ((*stack)->n == 0)
 	{
-		printf("L%u: division by zero\n", line_number);
+		fprintf(stderr, "L%u: division by zero\n", line_number);
 		free_mem(*stack);
 		exit(EXIT_FAILURE);
 	}
